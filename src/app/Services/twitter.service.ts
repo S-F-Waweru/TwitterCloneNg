450453a438b95f31ba8a5f058e55d212000b3c6d
Samuel Francis Waweru Ndegwa <s.f.waweru@gmail.com>
2024-06-28 15:52:07 +0300

    Initial Commit

---
+++ src/app/Services/twitter.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TwitterService {
+
+  constructor(private http:HttpClient) { }
+  private readonly User_Base_URL = "https://jsonplaceholder.typicode.com/users/"
+  private readonly Post_Base_URL = "https://jsonplaceholder.typicode.com/posts/"
+  private readonly Comments_Base_URL = "https://jsonplaceholder.typicode.com/comments/"
+
+
+
+  getAllUsers():Observable<User[]>{
+    return this.http.get<User[]>(this.User_Base_URL)
+  }
+  getUserPosts():Observable<Post[]>{
+    return this.http.get<Post[]>(this.Post_Base_URL)
+  }
+  getPostComments():Observable<Comment[]>{
+    return this.http.get<Comment[]>(this.Comments_Base_URL)
+  }
+}
+}
---
+++ src/app/display-posts/display-posts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-posts',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-posts.component.html',
+  styleUrl: './display-posts.component.css'
+})
+export class DisplayPostsComponent {
+
+}
